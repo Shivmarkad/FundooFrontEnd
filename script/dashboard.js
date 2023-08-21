ebfc5ff1b42ebc7e1cd6c406df744bc81036d3c3
Shivganesh <shivmarkad99@gmail.com>
2023-08-21 18:06:39 +0530

    [Shivganesh]Added: Note element to display every note when user creates

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,18 +1,59 @@
-const bar = document.getElementById('note_pop');
-const title = document.getElementById('title');
 const description = document.getElementById('description');
-const noteEle = document.querySelector('.note')
+const closeNote = document.getElementById('closeNote');
+const title = document.getElementById('title');
+const button_icons = document.getElementById('button_icons');
+const note = document.querySelector('.note');
+const allNotes = document.querySelector('.all_notes');
 
-description.addEventListener('focus', () => {
+function addnote(title, desc) {
+    let data = {
+        title: title,
+        description: desc
+    };
+    // $.ajax({
+    //     url:"http://localhost:3000/api/v1/note",
+    //     type:"POST",
+    //     data:data,
+    //     success: function(result){
+    //         console.log("Note created Successfully")
+    //         console.log(result)
+    //     }
+    // });
+
+    const htmlData = `<div class="note_element">
+    <span class="note_element_title">${title}</span>
+    <p class="note_element_desc">${desc}</p> 
+</div>`
+
+    allNotes.insertAdjacentHTML('beforeend', htmlData);
+    document.body.appendChild(allNotes);
+}
+
+description.addEventListener('click', () => {
     title.style.display = 'block';
-    bar.style.display = 'block';
-    noteEle.classList.add('after_focus');
+    button_icons.style.display = 'flex';
+    note.classList.add('after_focus');
 });
 document.addEventListener('click', (event) => {
-    // Check if the click is outside the input container
     if (!event.target.closest('.after_focus')) {
-        bar.style.display = 'none';
+        button_icons.style.display = 'none';
         title.style.display = 'none';
-        noteEle.classList.remove('after_focus');
+        note.classList.remove('after_focus');
     }
 });
+
+closeNote.addEventListener('click', () => {
+    const ttl = document.getElementById('title').value;
+    const desc = document.getElementById('description').value;
+
+    console.log(ttl, desc);
+
+    if (ttl == "" && desc == "") {
+        title.style.display = "none";
+        button_icons.style.display = "none";
+        note.classList.remove('after_focus');
+    } else {
+        addnote(ttl, desc);
+    }
+})
+
