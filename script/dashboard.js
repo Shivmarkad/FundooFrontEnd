0f6e202901e4c7a2bc86f6cfff32c88a739b8bd3
Shivganesh <shivmarkad99@gmail.com>
2023-08-23 13:05:47 +0530

    [Shivganesh] Added: Displayed notes created by user in dashboard integrated fetch all notes api with fronend

--- script/dashboard.js
+++ script/dashboard.js
@@ -6,30 +6,26 @@ const note = document.querySelector('.note');
 const allNotes = document.querySelector('.all_notes');
 
 function addnote(title, desc) {
+    let token = localStorage.getItem('token');
     let data = {
         title: title,
         description: desc
     };
-    let token = localStorage.getItem('token');
     $.ajax({
         url: "http://localhost:3000/api/v1/note",
         type: "POST",
-        headers: {
-            'Authorization': token
-        },
+        headers: {'Authorization': token},
         data: data,
         success: function (result) {
             console.log("Note created Successfully")
             console.log(result)
-        }
-    });
+        }});
 
     const htmlData = `<div class="note_element">
     <span class="note_element_title">${title}</span>
-    <p class="note_element_desc">${desc}</p> 
-</div>`
+    <p class="note_element_desc">${desc}</p></div>`
 
-    allNotes.insertAdjacentHTML('beforeend', htmlData);
+    allNotes.insertAdjacentHTML('afterBegin', htmlData);
     document.body.appendChild(allNotes);
 }
 
@@ -38,6 +34,7 @@ description.addEventListener('click', () => {
     button_icons.style.display = 'flex';
     note.classList.add('after_focus');
 });
+
 document.addEventListener('click', (event) => {
     if (!event.target.closest('.after_focus')) {
         button_icons.style.display = 'none';
@@ -58,23 +55,21 @@ closeNote.addEventListener('click', () => {
         note.classList.remove('after_focus');
     } else {
         addnote(ttl, desc);
-    }
-})
+    }})
 
 
-const colorPallet = document.getElementById('color_pallet');
-const colors = document.getElementById('colors');
+// const colorPallet = document.getElementById('color_pallet');
+// const colors = document.getElementById('colors');
+// const noteEle = document.querySelector('.note_element');
 
-const noteEle = document.querySelector('.note_element');
+// colorPallet.addEventListener('click', () => {
+//     colors.classList.toggle('hidden');
+// });
 
-colorPallet.addEventListener('click', () => {
-    colors.classList.toggle('hidden');
-});
-
-colors.addEventListener('click', (event) => {
-    const selectedColor = event.target.getAttribute('data-color');
-    if (selectedColor) {
-        noteEle.style.backgroundColor = selectedColor;
-        colors.classList.add('hidden');
-    }
-});
+// colors.addEventListener('click', (event) => {
+//     const selectedColor = event.target.getAttribute('data-color');
+//     if (selectedColor) {
+//         noteEle.style.backgroundColor = selectedColor;
+//         colors.classList.add('hidden');
+//     }
+// });
--- script/service.js
+++ script/service.js
@@ -1,17 +1,33 @@
 
 let token = localStorage.getItem('token');
-let array;
+var array;
 
 $.ajax({
-    url:"http://localhost:3000/api/v1/note",
-    type:"GET",
-    headers:{
-        'Authorization':token
+    url: "http://localhost:3000/api/v1/note",
+    type: "GET",
+    headers: {
+        'Authorization': token
     },
-    success: function(result){
+    success: function (result) {
         console.log("Notes fetched Successfully")
         array = result.data;
         console.log(array);
-
+        array.map(mapNotes);
     }
 })
+
+function mapNotes(item) {
+    const notes_area = document.getElementById("notes")
+    const noteEle = document.createElement("div")
+    const hdtag = document.createElement("h4")
+    hdtag.innerHTML = item.title;
+    const para = document.createElement("p")
+    para.innerHTML = item.description;
+
+    noteEle.appendChild(hdtag)
+    noteEle.appendChild(para)
+
+    noteEle.classList.add('note_element')
+
+    notes_area.appendChild(noteEle)
+}
